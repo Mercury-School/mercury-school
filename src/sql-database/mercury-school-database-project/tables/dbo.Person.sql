3d7b4e491b86b58c51dae3d5d28c6fff142c4dd0
Michael Henderson <mike@mohenderson.com>
2021-05-29 21:12:30 -0400

    Removed SQL Project.

--- src/sql-database/mercury-school-database-project/tables/dbo.Person.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[dbo.Person]
-(
-  Id int NOT NULL CONSTRAINT PK_Person PRIMARY KEY,
-  FirstName nvarchar(128) NOT NULL,
-  MiddleName nvarchar(128) NULL,
-  LastName nvarchar(128) NOT NULL,
-)
